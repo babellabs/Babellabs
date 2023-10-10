98922d3b1688598247bcfbd940f39b95a6b9b2de
[AtharvPorate1] <www.atharvporate254@gmail.com>
2023-10-10 13:04:43 +0530

    Added Wallet

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,14 +1,67 @@
 import React from 'react'
 import './navbar.css'
+import { useEffect, useState } from 'react';
 
 const Navbar = () => {
+
+  const [walletAddress, setWalletAddress] = useState(null);
+
+  const checkIfWalletIsConnected = async () => {
+    if (window?.solana?.isPhantom) {
+      console.log('Phantom wallet found!');
+      const response = await window.solana.connect({ onlyIfTrusted: true });
+      console.log(
+        'Connected with Public Key:',
+        response.publicKey.toString()
+      );
+
+      /*
+       * Set the user's publicKey in state to be used later!
+       */
+      setWalletAddress(response.publicKey.toString());
+    } else {
+      alert('Solana object not found! Get a Phantom Wallet 👻');
+    }
+  };
+
+  
+  
+
+  const connectWallet = async () => {
+
+    const { solana } = window;
+
+    if (solana) {
+      const response = await solana.connect();
+      console.log('Connected with Public Key:', response.publicKey.toString());
+      setWalletAddress(response.publicKey.toString());
+    }
+
+  };
+
+
+  const renderNotConnectedContainer = () => (
+    
+    <button className='wallet-btn' onClick={connectWallet}>Connnect Wallet</button>
+  );
+
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener('load', onLoad);
+    return () => window.removeEventListener('load', onLoad);
+  }, []);
+
+
   return (
     <div >
       <nav>
         <div className="Logo-box">BabelLabs<span className='logo-sm'>studio</span> </div>
-
+        
         <div className="wallet">
-            <button className='wallet-btn'>Connnect Wallet</button>
+
+        {!walletAddress && renderNotConnectedContainer()}
         </div>
       </nav>
     </div>
