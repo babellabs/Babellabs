72f644965476869728aea31dce07927346ebb2be
[AtharvPorate1] <www.atharvporate254@gmail.com>
2023-10-10 16:26:33 +0530

    Added Account image api

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -5,15 +5,22 @@ import { useEffect, useState } from 'react';
 const Navbar = () => {
 
   const [walletAddress, setWalletAddress] = useState(null);
+  const[account, setAccount] = useState(false);
+  const[publicKey, setPublicKey] = useState("");
+  
 
   const checkIfWalletIsConnected = async () => {
     if (window?.solana?.isPhantom) {
       console.log('Phantom wallet found!');
       const response = await window.solana.connect({ onlyIfTrusted: true });
+      const pk = response.publicKey.toString()
+      
       console.log(
         'Connected with Public Key:',
-        response.publicKey.toString()
+        pk
       );
+      setAccount(!account)
+      
 
       /*
        * Set the user's publicKey in state to be used later!
@@ -45,6 +52,14 @@ const Navbar = () => {
     <button className='wallet-btn' onClick={connectWallet}>Connnect Wallet</button>
   );
 
+  const renderConnectedAccountContainer = (publicKey) =>(
+    <div  className='accountContainer'>
+      <img src={`https://robohash.org/${publicKey}?size=50x50`} alt=""  />
+    </div>
+    
+    
+  );
+
   useEffect(() => {
     const onLoad = async () => {
       await checkIfWalletIsConnected();
@@ -62,6 +77,8 @@ const Navbar = () => {
         <div className="wallet">
 
         {!walletAddress && renderNotConnectedContainer()}
+        {account && renderConnectedAccountContainer()}
+        
         </div>
       </nav>
     </div>
